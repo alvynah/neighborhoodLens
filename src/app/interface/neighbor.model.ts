2fdd1254487ec4758b613aefd4906051a5065517
alvynah <alvynahw@gmail.com>
2021-06-09 02:12:07 +0300

    Auto-generated commit [ci skip]

--- src/app/interface/neighbor.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Neighbor {
-    id?: any;
-    name?: string;
-    location?: string;
-    description?: string;
-    health_tell?: number;
-    police_number?: number;
-    published?: boolean;
-}
--- src/app/interface/post.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Post {
-    id?: any;
-    title?: string;
-    post?: string;
-    date?: string;
-    user?: string;
-    hood?: string;
-    published?: boolean;
-}
