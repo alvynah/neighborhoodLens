1ffc700cf85e947be456197aab4c56ff2071f835
alvynah <alvynahw@gmail.com>
2021-06-08 22:34:08 +0300

    Add business components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBusinessComponent } from './components/add-business/add-business.component';
+import { BusinessComponent } from './components/business/business.component';
+import { BusinessDetailsComponent } from './components/business-details/business-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBusinessComponent,
+    BusinessComponent,
+    BusinessDetailsComponent
   ],
   imports: [
     BrowserModule,
